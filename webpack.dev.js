647948400cdac5ee09a2eddea5e09f49aad1456b
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-02-03 00:32:56 +0900

    Use babel

--- webpack.dev.js
+++ webpack.dev.js
@@ -41,6 +41,16 @@ module.exports = {
 						loader: 'sass-loader'
 					}
 				]
+			},
+			{
+				test: /\.m?js$/,
+				exclude: /node_modules/,
+				use: {
+					loader: 'babel-loader',
+					options: {
+						presets: ['@babel/preset-env']
+					}
+				}
 			}
 		]
 	},
