917656088ac40f5c7a41b57a3be2b6eff9ea9cb4
Tatsuya Sato <tatsuya.b.sato@rakuten.com>
2019-02-02 19:36:40 +0900

    Install bootstrap

--- src/index.js
+++ src/index.js
@@ -1,21 +1,26 @@
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap';
+
 import Chart from 'chart.js';
 import 'chartjs-plugin-colorschemes';
 import _ from 'lodash';
 
 function loadChart() {
     const ctx = document.getElementById('chart');
-    const data = _.times(5, () => _.random(0, 1, true));
+    const datasets = _.times(10, function(i) {
+        let data = _.times(5, () => _.random(0, 1, true));
+
+        return {
+            label: `Data ${i}`,
+            data: data
+        }
+    });
 
     return new Chart(ctx, {
         type: 'bar',
         data: {
             labels: ['A', 'B', 'C', 'D', 'E'],
-            datasets:[
-                {
-                    label: 'Sample',
-                    data: data
-                }
-            ]
+            datasets: datasets
         },
         options: {
             plugins: {
